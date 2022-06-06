68376ad4ea10fde3166ae1f9a894d94c004e5742
Daniella Guimaraes <danilgvet@gmail.com>
2022-06-06 12:01:27 -0300

    Curso em video - inicio

---
+++ Exemplos/app_00/app_00.js
@@ -0,0 +1,1 @@
+console.log('Meu primeiro programa em Javascript, executado pelo node.js!!!');
---
+++ Exemplos/app_02/app_02.js
@@ -0,0 +1,14 @@
+//Desenvolva um programa que peça dois números e imprima o maior deles
+
+let primeiroNumero = 1000;
+let segundoNumero = 100;
+
+let maior = 0;
+
+if (primeiroNumero > segundoNumero) {
+    maior = primeiroNumero;
+} else {
+    maior = segundoNumero;
+}
+
+console.log(maior);
